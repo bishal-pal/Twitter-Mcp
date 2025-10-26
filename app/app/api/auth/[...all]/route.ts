7340ef19fc3fb88cb7552fdd99d21d678c6c25dc
Bishal Pal <bishalpal686@gmail.com>
2025-10-26 19:17:35 +0530

    fix: auth path error

--- app/app/api/auth/[...all]/route.ts
+++ app/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
-import { auth } from "@/lib/auth"; // path to your auth file
+import { auth } from "@/app/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
 
